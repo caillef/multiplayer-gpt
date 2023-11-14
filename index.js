0bf6889bb077245622091a813d1e101a8882ec7d
Corentin Cailleaud <corentin.cailleaud@epitech.eu>
2023-11-14 21:28:20 +0400

    update

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const app = express();
 const port = 3000;
 
+app.use(express.json()); // Middleware to parse JSON body
+
 // Swagger set up
 const options = {
   definition: {
@@ -11,7 +13,7 @@ const options = {
     info: {
       title: 'Messages API',
       version: '1.0.0',
-      description: 'A simple API to get messages',
+      description: 'A simple API to get and post messages',
     },
   },
   apis: ['./index.js'], // files containing annotations for the OpenAPI Specification
@@ -42,6 +44,44 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
  */
 app.get('/messages', (req, res) => {
   res.json({ messages: ["First message", "Second message"] });
+});
+
+// POST /messages route
+/**
+ * @swagger
+ * /messages:
+ *   post:
+ *     summary: Post a new message
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ *                 example: "A new message"
+ *     responses:
+ *       200:
+ *         description: Message added successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 message:
+ *                   type: string
+ * */
+app.post('/messages', (req, res) => {
+  const newMessage = req.body.message;
+  if (!newMessage) {
+    return res.status(400).json({ success: false, message: "No message provided" });
+  }
+  // Here you would typically add the message to a database or in-memory storage
+  res.json({ success: true, message: newMessage });
 });
 
 app.listen(port, () => {
