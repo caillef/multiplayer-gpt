619d2e64e219bffd08b53a634dad5872c4320adc
Corentin Cailleaud <corentin.cailleaud@epitech.eu>
2023-11-14 21:34:30 +0400

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsdoc = require('swagger-jsdoc');
+const fs = require('fs');
+const yaml = require('js-yaml');
 const app = express();
 const port = 3000;
 
@@ -21,6 +23,10 @@ const options = {
 
 const openapiSpecification = swaggerJsdoc(options);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
+app.get('/api-docs/raw', (req, res) => {
+  res.send(yaml.dump(openapiSpecification))
+});
 
 // GET /messages route
 /**
