06d383742f8c27ab4ca7b552dcd3151d611aa623
Corentin Cailleaud <corentin.cailleaud@epitech.eu>
2023-11-14 21:41:55 +0400

    update

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ const options = {
       version: '1.0.0',
       description: 'A simple API to get and post messages',
     },
-    servers: {
+    servers: [{
       url: "https://multiplayer-gpt.caillef.com/"
-    }
+    }]
   },
   apis: ['./index.js'], // files containing annotations for the OpenAPI Specification
 };
@@ -32,10 +32,27 @@ app.get('/api-docs-raw', (req, res) => {
 
 // GET /messages route
 /**
+ The warnings you're encountering are related to some common requirements and best practices in OpenAPI (Swagger) documentation. Let's address these issues:
+
+1. **"In components section, schemas subsection is not an object"**: This error suggests that in your Swagger documentation, under the `components` section, the `schemas` subsection is not properly formatted as an object. The `schemas` subsection should be an object where each schema definition is a property.
+
+2. **"In path /messages, method get is missing operationId; skipping"** and **"In path /messages, method post is missing operationId; skipping"**: The `operationId` is a unique string used to identify an operation. If you don't provide it, some tools may not work correctly or might skip processing these paths.
+
+Here's how you can modify your existing Swagger comments to address these issues:
+
+- Ensure that the `schemas` subsection under `components` is properly formatted.
+- Add `operationId` to each route.
+
+Here's an example of how to adjust your comments:
+
+```javascript
+/**
  * @swagger
+ * 
  * /messages:
  *   get:
  *     summary: Retrieve a list of messages
+ *     operationId: getMessages
  *     responses:
  *       200:
  *         description: A list of messages.
@@ -49,7 +66,7 @@ app.get('/api-docs-raw', (req, res) => {
  *                   items:
  *                     type: string
  *                   example: ["First message", "Second message"]
- */
+ * */
 app.get('/messages', (req, res) => {
   res.json({ messages: ["First message", "Second message"] });
 });
@@ -57,19 +74,25 @@ app.get('/messages', (req, res) => {
 // POST /messages route
 /**
  * @swagger
+ * 
+ * components:
+ *   schemas:
+ *     Message:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ * 
  * /messages:
  *   post:
  *     summary: Post a new message
+ *     operationId: postMessage
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
- *             type: object
- *             properties:
- *               message:
- *                 type: string
- *                 example: "A new message"
+ *             $ref: '#/components/schemas/Message'
  *     responses:
  *       200:
  *         description: Message added successfully.
